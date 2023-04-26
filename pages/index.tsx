84edbec8a92e62927516b4b055f1b6c328172dea
Matt Nelsen <mattnelsen95@gmail.com>
2023-04-26 15:09:59 -0700

    Adding large html/css changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,81 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ["latin"] });
+// const fontRobotoRegular = localFont({ src: "../fonts/Roboto-Regular.ttf" });
+const fontRobotoRegular = localFont({ src: "../fonts/Roboto-Light.ttf" });
 
 export default function Home() {
+  const links = [
+    { href: "", description: "Resume" },
+    { href: "https://github.com/m-nelsen", description: "GitHub" },
+    {
+      href: "https://www.linkedin.com/in/matthew-nelsen/",
+      description: "Linkedin",
+    },
+  ];
+
+  const svgContainer = Array(10).fill({});
+
   return (
     <>
       <Head>
         <title>Matt Nelsen</title>
-        <meta name="description" content="Matt Nelsen Web Developer Portfolio" />
+        <meta
+          name="description"
+          content="Matt Nelsen Web Developer Portfolio"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main className={fontRobotoRegular.className}>
+        <section>
+          <h1>Matt Nelsen</h1>
+          <h2>Web Developer</h2>
+        </section>
+        <section>
+          <ul>
+            {links.map((link, index) => {
+              const { href, description } = link;
+
+              return (
+                <li key={index}>
+                  <a href={href} target="_blank">
+                    {description}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+      </main>
+      <div>
+        <ul className="animated-svg-container">
+          {svgContainer.map((e, index) => {
+            const height = Math.random() * 100 + 50;
+            const width = Math.random() * 100 + 50;
+
+            return (
+              <svg
+                key={index}
+                width={width}
+                height={height}
+                className="svg-rect-wrapper"
+                style={{
+                  animationDelay: `${index}s`,
+                  left: `${Math.random() * 90 + 10}%`,
+                }}
+              >
+                <rect
+                  width={width}
+                  height={height}
+                  className="svg-rect"
+                  fill="#2c334c"
+                />
+              </svg>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 }
